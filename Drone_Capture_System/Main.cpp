960d90566c3c4b397f371e79472ca95e9b6b7cbf
MyrtheVanMoorst <65168254+MyrtheVanMoorst@users.noreply.github.com>
2021-12-06 16:34:59 +0100

    changes

--- Drone_Capture_System/Main.cpp
+++ Drone_Capture_System/Main.cpp
@@ -2,7 +2,25 @@
 
 int main() {
 
-		imageToSave = captureClass.initialize();
-		saveClass.saveImage(imageToSave);
+	while (true)
+	{
+
+		if(gpsrange>setRange)saveClass.saveImage(captureClass.capureRGB, "RGB");
+
+
+
+		imageToSaveNDVI = captureClass.initialize();
+		saveClass.saveImage(imageToSaveNDVI, ct);
+
+		ct++;
+
+		if (waitKey(1) == 'c') {
+
+			break;
+		}
+		///update telemetry 
+
+	}
+	//telemetry loop
 
 }
